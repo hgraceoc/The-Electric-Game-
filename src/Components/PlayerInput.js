e2ee646e627f3922b968e17ac7920d37dd7e2216
Hannah Cryer <hannahcryer@hotmail.com>
2020-06-19 22:59:29 +0100

    Placeholder Background Colours

--- src/Components/PlayerInput.js
+++ src/Components/PlayerInput.js
@@ -10,15 +10,49 @@ function PlayerInput() {
                 <form>
                     <h1 className="formTitle">Name Your Players</h1>
                     <div className="formGroup">
-                        <input 
-                        type="text" 
-                        class="input"
-                        value="">
+                        <input
+                            type="text"
+                            class="input"
+                            value=""
+                            placeholder="#1">
                         </input>
-                        <input type="text" class="input"></input>
-                        <input type="text" class="input"></input>
+
+                        <input
+                            type="text"
+                            class="input"
+                            value=""
+                            placeholder="#2">
+                        </input>
+
+                        <input
+                            type="text"
+                            class="input"
+                            value=""
+                            placeholder="#3">
+                        </input>
+
+                        <input
+                            type="text"
+                            class="input"
+                            value=""
+                            placeholder="#4">
+                        </input>
+
+                        <input
+                            type="text"
+                            class="input"
+                            value=""
+                            placeholder="#5">
+                        </input>
+
+                        <input
+                            type="text"
+                            class="input"
+                            value=""
+                            placeholder="#6">
+                        </input>
                     </div>
-                    <button className="formButton">Pick My Team</button>
+                    <button className="formButton">Pick My Teams</button>
                 </form>
             </div>
         </div>
