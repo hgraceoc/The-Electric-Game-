ac32b60cdf6823b4019b4cd0127e432bb187932c
Hannah Cryer <hannahcryer@hotmail.com>
2020-06-19 20:54:47 +0100

    Added a homepage Component

---
+++ src/Components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ball from '../../src/ball.svg';
+
+
+function Home() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ball} className="App-logo" alt="logo" />
+          
+          <p>I hear that people call it the Electric Game</p>
+  
+        </header>
+      </div>
+    );
+  }
+  
+  export default Home;
