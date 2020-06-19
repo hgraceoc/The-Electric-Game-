f94a4ce4d354d76b9886da99f2b6a2a6d8dde93f
Hannah Cryer <hannahcryer@hotmail.com>
2020-06-19 20:56:21 +0100

    Updated App.js to reflect new routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ball from './ball.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from "react-router-dom";
 import './App.css';
+
+import Home from "./Components/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ball} className="App-logo" alt="logo" />
-        
-        <p>I hear that people call it the Electric Game</p>
+    <Home></Home>
 
-      </header>
-    </div>
   );
 }
 
