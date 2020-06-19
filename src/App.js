d6815941ef5e21d451f2ef59228530abec3ee924
Hannah Cryer <hannahcryer@hotmail.com>
2020-06-19 17:47:28 +0100

    Set Up React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import ball from './ball.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={ball} className="App-logo" alt="logo" />
+        
+        <p>I hear that people call it the Electric Game</p>
+
       </header>
     </div>
   );
