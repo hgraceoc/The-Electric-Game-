16131cd752b4d78164f511401ee5a54964d67830
Hannah Cryer <hannahcryer@hotmail.com>
2020-06-26 17:48:41 +0100

    Edit Mobile Styling

--- src/Components/PlayerInput/PlayerInput.js
+++ src/Components/PlayerInput/PlayerInput.js
@@ -63,15 +63,15 @@ class PlayerInput extends Component {
         this.props.handleRemovePlayer(playerName.id)
     }
 
-   //helper function to split players into two equal teams of 5
-   pickTeams = (players, numberOfPlayers) => {
-       let teams = [], i = 0, n = players.length;
+    //helper function to split players into two equal teams of 5
+    pickTeams = (players, numberOfPlayers) => {
+        let teams = [], i = 0, n = players.length;
         while (i < n) {
-          teams.push(players.slice(i, i += numberOfPlayers));
+            teams.push(players.slice(i, i += numberOfPlayers));
         }
         return teams;
     }
-    
+
 
 
     render() {
@@ -91,7 +91,7 @@ class PlayerInput extends Component {
             return <h3>{player}</h3>
         })
 
-    
+
 
         //displayed below:
         // reset button - take user back to empty form 
@@ -101,105 +101,104 @@ class PlayerInput extends Component {
         // randomise team button - submits form, which then hides form and displays two team tables 
         return (
             <>
-                    {!submitted ? (
-                        <div className="playerInputForm">
+                {!submitted ? (
+                    <div className="playerInputForm">
 
-                            <div className="form">
-                                <form onSubmit={this.handleSubmit}>
-                                    <h1 className="title">Name Your Players</h1>
-                                    <p>Enter <strong>10 Player Names</strong> & Then Click <strong>'Randomise Teams'</strong> To Generate Two Random Teams..</p>
-                                    <div className="formGroup">
-                                        <input
-                                            onChange={this.handleChange}
-                                            value={playerName}
-                                            placeholder="Enter Player Name.."
-                                            required>
-                                        </input>
+                        <div className="form">
+                            <form onSubmit={this.handleSubmit}>
+                                <h1 className="title">Name Your Players</h1>
+                                <p>Enter <strong>10 Player Names</strong> & Then Click <strong>'Randomise Teams'</strong> To Generate Two Random Teams..</p>
+                                <div className="formGroup">
+                                    <input
+                                        onChange={this.handleChange}
+                                        value={playerName}
+                                        placeholder="Enter Player Name.."
+                                        required>
+                                    </input>
 
-                                    </div>
-                                    <button
-                                        className="formButton"
-                                        type="submit">
-                                        Add Player</button>
-                                </form>
-                                <ul className="playerList">
-                                    {players.map((player, index) =>
-                                        <>
-                                            <li
-                                                key={index}
-                                                className="playerListItem">
-                                                <label className="playerLabel">Player{index + 1} :  </label>
-                                                {player}
-                                            </li>
-                                            <button
-                                                className="removePlayerButton"
-                                                onClick={() => this.handleRemovePlayer(playerName)}>Remove Player
+                                </div>
+                                <button
+                                    className="formButton"
+                                    type="submit">
+                                    Add Player</button>
+                            </form>
+                            <ul className="playerList">
+                                {players.map((player, index) =>
+                                    <>
+                                        <li
+                                            key={index}
+                                            className="playerListItem">
+                                            <label className="playerLabel">Player{index + 1} :  </label>
+                                            {player}
+                                        </li>
+                                        <button
+                                            className="removePlayerButton"
+                                            onClick={() => this.handleRemovePlayer(playerName)}>Remove Player
                                     </button>
-                                        </>
-                                    )}
-                                </ul>
-                            </div>
+                                    </>
+                                )}
+                            </ul>
                         </div>
-                    ) : null}
+                    </div>
+                ) : null}
 
 
-                    
-                        <div className="submitTeamButtonContainer">
-                        {!submitted && players.length === 10 ? (  
-                            <button
-                                onClick={this.handleSubmitTeams}
-                                className="formButton submitTeamButton"
-                                type="submit">
-                                Randomise Teams!
-                            </button> ) : null}
 
-                            <button
-                                onClick={this.handleReset}
-                                className="formButton resetButton1"
-                                type="submit">
-                                Reset!
+                <div className="submitTeamButtonContainer">
+                    {!submitted && players.length === 10 ? (
+                        <button
+                            onClick={this.handleSubmitTeams}
+                            className="formButton submitTeamButton"
+                            type="submit">
+                            Randomise Teams!
+                        </button>) : null}
+
+                    <button
+                        onClick={this.handleReset}
+                        className="formButton resetButton1"
+                        type="submit">
+                        Reset!
                             </button>
-                            </div>
+                </div>
 
 
-                    {submitted ? (
-                        <>
-                            <div className="teamTables">
+                {submitted ? (
+                    <>
+                        <div className="teamTables">
+                            <table className="teamTable">
+                                <thead>
+                                    <tr>
+                                        <th className="title">Team One</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    <tr>
+                                        {teamOne}
+                                    </tr>
+
+                                </tbody>
+                            </table>
+
+                            
                                 <table className="teamTable">
                                     <thead>
                                         <tr>
-                                            <th className="title">Team One</th>
+                                            <th className="title">Team Two</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        
-                                            <tr>
-                                               {teamOne}
-                                            </tr>
-                                       
+
+                                        <tr>
+                                            {teamTwo}
+                                        </tr>
+
                                     </tbody>
                                 </table>
-
-                                <div>
-                                    <table className="teamTable">
-                                        <thead>
-                                            <tr>
-                                                <th className="title">Team Two</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            
-                                                <tr>
-                                                    {teamTwo}
-                                                    
-                                                </tr>
-                                        
-                                        </tbody>
-                                    </table>
-                                </div>
                             </div>
+                        
 
-                            <div className="buttons">                
+                        <div className="buttons">
                             <button
                                 onClick={this.handleEditPlayers}
                                 className="formButton editButton"
@@ -214,9 +213,9 @@ class PlayerInput extends Component {
                                 Reset!
                             </button>
 
-                            </div>  
-                        </>
-                    ) : null}
+                        </div>
+                    </>
+                ) : null}
             </>
         )
     }
