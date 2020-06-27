9632979ecc9c23b31dddd9526394e8043c2ede8b
Hannah Cryer <hannahcryer@hotmail.com>
2020-06-27 16:00:50 +0100

    Updates

---
+++ precache-manifest.6a86c80292e794583b026004c0843209.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1adcf4e87af7f8fd49a265756da251",
+    "url": "/The-Electric-Game-/index.html"
+  },
+  {
+    "revision": "6af26192cb5c51dcd354",
+    "url": "/The-Electric-Game-/static/css/main.d9e52e67.chunk.css"
+  },
+  {
+    "revision": "165139d9fed6facd5e99",
+    "url": "/The-Electric-Game-/static/js/2.b21629a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/The-Electric-Game-/static/js/2.b21629a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af26192cb5c51dcd354",
+    "url": "/The-Electric-Game-/static/js/main.627ad7b2.chunk.js"
+  },
+  {
+    "revision": "3ca616d647c04d9d752b",
+    "url": "/The-Electric-Game-/static/js/runtime-main.f5ec484d.js"
+  },
+  {
+    "revision": "01d1677e091850886b4a79d2e45563bc",
+    "url": "/The-Electric-Game-/static/media/Merriweather-Bold.01d1677e.ttf"
+  },
+  {
+    "revision": "db0192bf3558b950161f74432b425973",
+    "url": "/The-Electric-Game-/static/media/Muli-Italic.db0192bf.ttf"
+  },
+  {
+    "revision": "5ce4272c965d67353f25256ac70a0bfa",
+    "url": "/The-Electric-Game-/static/media/Shrikhand-Regular.5ce4272c.ttf"
+  },
+  {
+    "revision": "b7f915a00865ec36e8dc30b032499475",
+    "url": "/The-Electric-Game-/static/media/background.b7f915a0.jpg"
+  },
+  {
+    "revision": "ade08109a1bd787d9c5bf81c84a430f9",
+    "url": "/The-Electric-Game-/static/media/football.ade08109.svg"
+  }
+]);
